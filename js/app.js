f0cb261fae154d143ddd00491b56ac51e5ef9e0b
Md-shefat-masum <mshefat924@gmail.com>
2020-11-01 09:06:27 +0600

    responsive done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 const hamburger = document.querySelector('.hamburgers');
 const navAllLinks = document.querySelector('.nav-all-links');
 
-console.log(hamburger, navAllLinks);
-
 hamburger.addEventListener('click', () => {
-    console.log('clicked');
     navAllLinks.classList.toggle('open')
 })
