36760d0af68ed7b9fac716a7b180495e8c1fbb54
Md-shefat-masum <mshefat924@gmail.com>
2020-12-03 08:44:03 +0600

    dynamic with js header and banner

---
+++ js/siteData.js
@@ -0,0 +1,68 @@
+let siteData = {
+    top_gretting: {
+        content: 'Welcome to my site',
+        section_name: 'header section'
+    },
+    header_social_links: {
+        facebook: {
+            icon: 'fab fa-facebook-f',
+            link: 'https://facebook.com'
+        },
+        twitter: {
+            icon: 'fab fa-twitter',
+            link: 'https://twitter.com'
+        },
+        linkedin: {
+            icon: 'fab fa-linkedin',
+            link: 'https://linkedin.com'
+        },
+        github: {
+            icon: 'fab fa-github',
+            link: 'https://github.com'
+        },
+    },
+
+    banner: {
+        banner1: {
+            image : 'https://png.pngtree.com/thumb_back/fh260/back_pic/02/50/63/71577e1cf59d802.jpg',
+            h2 : 'Hi! I\'m ',
+            h1 : 'UI\'UX Designer',
+            p : 'Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic',
+            hire_us : '#contact',
+            contact_us : '#contact',
+        },
+        banner2: {
+            image : 'https://image.freepik.com/free-vector/abstract-banner-background-design_1048-10876.jpg',
+            h2 : 'Hi! I\'m ',
+            h1 : 'UI\'UX Designer',
+            p : 'Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic',
+            hire_us : '#contact',
+            contact_us : '#contact',
+        },
+        banner3: {
+            image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHcsSHqITxHRfAkmTAwMqZXTAyAVNPaq8aJQ&usqp=CAU',
+            h2 : 'Hi! I\'m ',
+            h1 : 'UI\'UX Designer',
+            p : 'Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic',
+            hire_us : '#contact',
+            contact_us : '#contact',
+        },
+    },
+
+    header_logo: {
+        content: 'images/site_image/logo.png'
+    },
+
+    about_us_heading: {
+        content: 'About Me'
+    },
+    aboutme_p_1: {
+        content: 'hi this is me, i\'m a web frontend developer. Free Nationwide Talk and Text service. TextNow is the only app that offers free nationwide cellular service supported by ads. Just buy a SIM Activation Kit to get'
+    },
+    aboutme_p_2: {
+        content: 'Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or other type of compatible computer.'
+    },
+    our_service_heading: {
+        content: 'My Services'
+    }
+};
---
+++ js/siteDataController.js
@@ -0,0 +1,77 @@
+// console.log(siteData);
+
+// console.log(siteData.three);
+// console.log(siteData.two);
+
+for (const key in siteData) {
+    if (siteData.hasOwnProperty(key)) {
+        const element = siteData[key];
+
+        if(key === 'header_logo'){
+            $(`#${key}`).attr('src',element.content);
+
+        }else if(key === 'header_social_links'){
+            // console.log(key, element);
+            let social_icons_list = '';
+            for (const slink in element) {
+                if (element.hasOwnProperty(slink)) {
+                    const element2 = element[slink];
+                    social_icons_list += `<li><a href="${element2.link}"><i class="${element2.icon}"></i></a></li>`;
+                    // console.log(element2,social_icons_list);
+                    $('#header_social_links').html(social_icons_list);
+                }
+            }
+
+        }else if(key === 'banner'){
+            // console.log(key,element);
+            let banner_list = '';
+            for (const banner in element) {
+                if (element.hasOwnProperty(banner)) {
+                    const element2 = element[banner];
+                    banner_list += `
+                            <div style="
+                                    background-image: url('${element2.image}');
+                                    background-repeat: no-repeat;
+                                    background-size: cover;
+                                ">
+                            <div class="overlay">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-md-6 col-sm-0"></div>
+                                        <div class="col-md-6">
+                                            <div class="banner-content">
+                                                <h2>${element2.h2}</h2>
+                                                <h1>${element2.h1}</h1>
+                                                <p>
+                                                    ${element2.p}
+                                                </p>
+                                                <ul>
+                                                    <li><a href="${element2.hire_us}">Hire Us</a></li>
+                                                    <li><a href="${element2.contact_us}">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                    // console.log(banner_list);
+                    $('#banner').html(banner_list);
+                }
+            }
+        }
+        else{
+            $(`#${key}`).html(element.content);
+        }
+
+
+        // $('#'+key).html(element.content);
+
+        // console.log( key, element );
+
+        // console.log($(`#${key}`));
+    }
+}
+
+// $('#top_gretting').html('test')
